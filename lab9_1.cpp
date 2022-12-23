57c38fd6193ab599898958c8bee7743a2cacef49
TRex612 <poochit.moonkaew@gmail.com>
2022-12-23 16:29:02 +0700

    cp

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 
 int main()
 {
   char rank;
   cout << "Input your rank: ";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
+  string x,y,z,i,j;
+    x = "You have received Super Ultra Rare Unit!!!\n";
+    y ="You have received 5 gems.\n";
+    z = "You have received 1 gems.\n";
+    i = "You have received 2000 coins.\n";
+    j = "You have received very KAK items.\n";
+
+  if (rank == 'S'  )
+  {
+    cout << x<<y<<z<<i<<j;
   }
-  return 0;
+  else if (rank == 'A' )
+  {
+    cout << y<<z<<i<<j;
+  }
+  else if (rank == 'B' )
+  {
+    cout << z<<i<<j;
+  }
+  else if (rank == 'C' )
+  {
+    cout <<i<<j;
+  }
+  else if (rank == 'D')
+  {
+    cout << j;
+  }
+  
 }
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,3 +11,66 @@
 "Brook"
 "Jinbe"
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age,height,bounty;
+    string namecharacter[] = {"Zoro","Sanji","Usopp","Chopper","Jinbe","Franky","Brook"} ,namechar;
+
+    cout << "Enter your age: ";
+    cin >> age ;
+
+    if (age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> height ;
+        if (height<100)
+        {
+            namechar = namecharacter[3];
+        }
+        else if (height<180)
+        {
+            namechar = namecharacter[2];
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bounty ;
+            if (bounty > 1100000000)
+            {
+                namechar = namecharacter[0];
+            }
+            else{
+                namechar = namecharacter[1];
+            }
+            
+        }
+        
+        
+    }
+    else{
+        if (age <= 60)
+        {
+            cout << "Enter your bounty: ";
+            cin >> bounty ;
+            if (bounty>500000000)
+            {
+                namechar = namecharacter[4];
+            }
+            else{
+                namechar = namecharacter[5];
+            }
+            
+        }
+        else{
+            namechar = namecharacter[6];
+        }
+        
+
+    }
+    
+    cout << "Your character = " << namechar ;
+
+}
